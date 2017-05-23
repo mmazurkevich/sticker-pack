08e872411fc4a3ee94318f8becdef94c5a3d4d4c
InfinityQX70s <mazumisha@gmail.com>
2017-05-24 00:26:24 +0300

    Add bootstrap template

---
+++ src/main/java/org/sticker/pack/AuthorizationController.java
@@ -0,0 +1,21 @@
+package org.sticker.pack;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Mikhail on 23.05.2017.
+ */
+@Controller
+public class AuthorizationController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/lol")
+    public String lol() {
+        return "index";
+    }
+}
---
+++ src/main/java/org/sticker/pack/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package org.sticker.pack;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Mikhail on 24.05.2017.
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+}
