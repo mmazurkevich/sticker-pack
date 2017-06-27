3c4d096ae85999f8deefa7016193ea5a98b74537
InfinityQX70s <mazumisha@gmail.com>
2017-06-28 01:34:59 +0300

    Add Facebook button

--- src/main/java/org/sticker/pack/controller/AuthorizationController.java
+++ src/main/java/org/sticker/pack/controller/AuthorizationController.java
@@ -1,7 +1,17 @@
 package org.sticker.pack.controller;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 
 /**
  * Created by Mikhail on 23.05.2017.
@@ -13,4 +23,39 @@ public class AuthorizationController {
     public String loginPage() {
         return "login";
     }
+
+    @PostMapping("/tokensignin")
+    public String googleAuth(@RequestParam("id_token") String token) throws GeneralSecurityException, IOException {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier
+                .Builder(new NetHttpTransport(), new JacksonFactory())
+                .setAudience(Collections.singletonList("89944683825-852048csojanv1vacf88te428nuuhj82.apps.googleusercontent.com"))
+                // Or, if multiple clients access the backend:
+                //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                .build();
+
+// (Receive idTokenString by HTTPS POST)
+
+        GoogleIdToken idToken = verifier.verify(token);
+        if (idToken != null) {
+            GoogleIdToken.Payload payload = idToken.getPayload();
+
+            // Print user identifier
+            String userId = payload.getSubject();
+            System.out.println("User ID: " + userId);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+        } else {
+            System.out.println("Invalid ID token.");
+        }
+        System.out.println(token);
+        return "redirect:/sticker";
+    }
+
 }
