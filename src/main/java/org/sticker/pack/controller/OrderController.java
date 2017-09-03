3133e0c07a77dc3b38c015e3e47bc3b6600dca7a
InfinityQX70s <mazumisha@gmail.com>
2017-09-03 23:21:02 +0300

    Add model atribute wrapper

--- src/main/java/org/sticker/pack/controller/OrderController.java
+++ src/main/java/org/sticker/pack/controller/OrderController.java
@@ -1,18 +1,18 @@
 package org.sticker.pack.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.sticker.pack.controller.dto.OrderInfoWrapper;
 import org.sticker.pack.model.Customer;
 import org.sticker.pack.service.CustomerService;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 /**
  * Created by Mikhail on 13.08.2017.
@@ -26,19 +26,20 @@ public class OrderController {
     @GetMapping("/order")
     private String getOrderPage(HttpSession session, Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth instanceof AnonymousAuthenticationToken) {
-            return "redirect:/";
-        } else {
+//        if (auth instanceof AnonymousAuthenticationToken) {
+//            return "redirect:/";
+//        } else {
             Customer customer = customerService.find(auth.getPrincipal().toString());
             model.addAttribute("customer", customer);
             return "order";
-        }
+//        }
     }
 
     @PostMapping("/order")
-    private String createOrder(HttpSession session, Model model) {
+    private String createOrder(@ModelAttribute OrderInfoWrapper orderInfo) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         auth.getPrincipal().toString();
+        System.out.println(orderInfo);
         return "order";
     }
 }
--- src/main/java/org/sticker/pack/controller/dto/OrderInfoWrapper.java
+++ src/main/java/org/sticker/pack/controller/dto/OrderInfoWrapper.java
@@ -4,4 +4,91 @@ package org.sticker.pack.controller.dto;
  * Created by Mikhail on 13.08.2017.
  */
 public class OrderInfoWrapper {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String region;
+    private String city;
+    private String deliveryAddress;
+    private String postIndex;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getPostIndex() {
+        return postIndex;
+    }
+
+    public void setPostIndex(String postIndex) {
+        this.postIndex = postIndex;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderInfoWrapper{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", region='" + region + '\'' +
+                ", city='" + city + '\'' +
+                ", deliveryAddress='" + deliveryAddress + '\'' +
+                ", postIndex='" + postIndex + '\'' +
+                '}';
+    }
 }
