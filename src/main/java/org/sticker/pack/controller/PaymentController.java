0ab1cc4f9ab88fde00d224ae2a4ce3b18bd3ff2f
InfinityQX70s <mazumisha@gmail.com>
2017-09-14 00:40:49 +0300

    Add sendbox payment

--- src/main/java/org/sticker/pack/controller/PaymentController.java
+++ src/main/java/org/sticker/pack/controller/PaymentController.java
@@ -1,10 +1,15 @@
 package org.sticker.pack.controller;
 
-import com.braintreegateway.BraintreeGateway;
-import com.braintreegateway.Environment;
+import com.braintreegateway.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.PrintStream;
+import java.math.BigDecimal;
 
 /**
  * Created by Mikhail on 13.09.2017.
@@ -24,5 +29,28 @@ public class PaymentController {
     private String getPaymentPage(Model model) {
         model.addAttribute("clientToken", gateway.clientToken().generate());
         return "dropin";
+    }
+
+    @PostMapping("/checkout")
+    private String getPaymentPage(@RequestBody String nonceFromTheClient) {
+        TransactionRequest request = new TransactionRequest()
+                .amount(new BigDecimal("10.00"))
+                .paymentMethodNonce(nonceFromTheClient)
+                .options()
+                .submitForSettlement(true)
+                .done();
+
+        Result<Transaction> result = gateway.transaction().sale(request);
+
+        if (!result.isSuccess()){
+            ValidationErrors errors = result.getErrors();
+            errors.getAllValidationErrors().forEach(System.out::println);
+//            Transaction transaction = result.getTransaction();
+//
+//            System.out.println(transaction.getStatus());
+//            System.out.println(transaction.getProcessorResponseCode());
+//            System.out.println(transaction.getProcessorResponseText());
+        }
+        return "redirect:/payment";
     }
 }
