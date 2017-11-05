c167900bfc915cf62b973cb634db5b66e0703dec
mmazurkevich <mazumisha@gmail.com>
2017-11-05 23:54:26 +0300

    Add iziToast library

--- src/main/resources/static/js/sticker-shop.js
+++ src/main/resources/static/js/sticker-shop.js
@@ -1,8 +1,20 @@
+//Enable tooltips
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
 $(".btn-wish-list").click(function() {
     if($(this).attr("class").includes("btn-wish-list-active")) {
-        $(this).removeClass('btn-wish-list-active')
+        $(this).removeClass('btn-wish-list-active');
+        iziToast.error({
+            title: 'Error',
+            message: 'Illegal operation'
+        });
     } else {
         $(this).addClass('btn-wish-list-active');
-        $(".alert").alert()
+        iziToast.success({
+            title: 'OK',
+            message: 'Successfully inserted record!'
+        });
     }
 });
