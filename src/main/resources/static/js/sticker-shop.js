357e0e8a82291aa20efa66272b399b07bd8ae3ea
mmazurkevich <mazumisha@gmail.com>
2017-11-08 19:07:08 +0300

    Start iziToast customisation

--- src/main/resources/static/js/sticker-shop.js
+++ src/main/resources/static/js/sticker-shop.js
@@ -1,20 +1,35 @@
 //Enable tooltips
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
+    iziToast.settings({
+        zindex: 2000,
+        timeout: 500000,
+        position: 'topRight',
+        progressBar: false
+    });
 });
 
 $(".btn-wish-list").click(function() {
     if($(this).attr("class").includes("btn-wish-list-active")) {
         $(this).removeClass('btn-wish-list-active');
         iziToast.error({
-            title: 'Error',
-            message: 'Illegal operation'
+            color: '#fdebeb',
+            titleColor: '#f5514f',
+            iconColor: '#f5514f',
+            messageColor: '#f5514f',
+            title: 'Product',
+            message: 'removed from your wishlist!'
         });
     } else {
         $(this).addClass('btn-wish-list-active');
-        iziToast.success({
-            title: 'OK',
-            message: 'Successfully inserted record!'
+        iziToast.info({
+            icon: 'fa fa-bell-o',
+            color: '#e8f7fd',
+            titleColor: '#60c3e9',
+            iconColor: '#60c3e9',
+            messageColor: '#60c3e9',
+            title: 'Product',
+            message: 'added to your wishlist!'
         });
     }
 });
